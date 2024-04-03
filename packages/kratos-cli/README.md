9d4690f2161fb1c5d7d67a7ee2ea503f99fdd301
getlarge <ed@getlarge.eu>
2024-04-03 18:51:56 +0200

    chore: init kratos-cli

---
+++ packages/kratos-cli/README.md
@@ -0,0 +1,11 @@
+# kratos-cli
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build kratos-cli` to build the library.
+
+## Running unit tests
+
+Run `nx test kratos-cli` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/kratos-cli/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'kratos-cli',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/kratos-cli',
+};
