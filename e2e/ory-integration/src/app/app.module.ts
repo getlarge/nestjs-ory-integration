ca75a3d80df27494dac10114be3f8d6ae95ba665
getlarge <ed@getlarge.eu>
2025-02-05 10:32:52 +0100

    chore: integrate Ory modules for OAuth2 and permissions in app module

--- e2e/ory-integration/src/app/app.module.ts
+++ e2e/ory-integration/src/app/app.module.ts
@@ -1,10 +1,23 @@
+import { OryOAuth2Module } from '@getlarge/hydra-client-wrapper';
+import { OryPermissionsModule } from '@getlarge/keto-client-wrapper';
+import { OryFrontendModule } from '@getlarge/kratos-client-wrapper';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    OryPermissionsModule.forRoot({
+      basePath: process.env['ORY_KETO_PUBLIC_URL'] ?? 'http://localhost:4466',
+    }),
+    OryOAuth2Module.forRoot({
+      basePath: process.env['ORY_HYDRA_PUBLIC_URL'] ?? 'http://localhost:4444',
+    }),
+    OryFrontendModule.forRoot({
+      basePath: process.env['ORY_KRATOS_PUBLIC_URL'] ?? 'http://localhost:4433',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
